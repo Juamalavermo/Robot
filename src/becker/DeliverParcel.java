a070139c229869e2f42c0db963ee334852bc7b13
Estudiante <Estudiante@10.203.3.1>
2017-08-01 16:03:36 -0500

    robot

--- src/becker/DeliverParcel.java
+++ src/becker/DeliverParcel.java
@@ -8,10 +8,29 @@ public class DeliverParcel
    {  
      // Set up the initial situation
       City prague = new City();
-      Thing parcel = new Thing(prague, 1, 2);
-      Robot karel = new Robot(prague, 1, 0, Direction.EAST,5);
-    
+      Thing t1 = new Thing(prague, 1, 2);
+      Thing t2 = new Thing(prague, 2, 2);
+      Thing t3 = new Thing(prague, 2, 3);
+      Thing t4 = new Thing(prague, 2, 4);
+      Thing t5 = new Thing(prague, 1, 2);
+      Thing t6 = new Thing(prague, 2, 2);
+      Thing t7 = new Thing(prague, 1, 3);
+      Thing t8 = new Thing(prague, 3, 4);
+      Thing t9 = new Thing(prague, 3, 3);
+      Robot karel = new Robot(prague, 1, 0, Direction.EAST,20);
+      Wall P1 = new Wall(prague, 1, 2, Direction.WEST);
       // Direct the robot to the final situation
+      //Si puedo tomar cosa
+      //karel.canPickThing();
+      //Coja algo
+      //karel.pickThing();
+      //ponga una
+      //karel.putThing();
+      //Moverse
+      //karel.move();
+      //karel.turnLeft();
+      //karel.frontIsClear();
+      
       karel.move();
       karel.move();
       karel.pickThing();
